3f0d96c3dd88eb2b224c5f2cb21d4650d2a35622
DavidEggenberger <david99.eggenberger@gmail.com>
2022-11-11 23:04:29 +0100

    API Setup

---
+++ WebAPI/Controllers/CityServicesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CityServicesController : ControllerBase
+    {
+        private readonly ILogger<CityServicesController> _logger;
+
+        public CityServicesController(ILogger<CityServicesController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+    }
+}
