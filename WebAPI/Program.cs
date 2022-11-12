4b236382febf573b12ee9e7d75ac87e3aa5c0f74
DavidEggenberger <david99.eggenberger@gmail.com>
2022-11-12 11:02:49 +0100

    Rename to WebClient

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -29,7 +29,7 @@ namespace WebAPI
             Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((hostingContext) =>
                 {
-                    hostingContext.AddAzureKeyVault(new Uri("https://vadianvibes.vault.azure.net"),
+                    hostingContext.AddAzureKeyVault(new Uri("https://vadianvibes.vault.azure.net/"),
                             new DefaultAzureCredential(new DefaultAzureCredentialOptions { ManagedIdentityClientId = "e735d2bc-2070-46b8-bf44-4fa9b69f68e1" }));
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
